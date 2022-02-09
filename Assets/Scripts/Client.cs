cecec9e948b81b1823c33b06128ab3d5cd24625b
v_hutsuliak <v_hutsuliak@ugi-studio.com>
2022-02-09 21:52:55 +0200

    added simple server logic

---
+++ Assets/Scripts/Client.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net.Sockets;
+using UnityEngine;
+
+namespace GameServer
+{
+    public class Client
+    {
+        private static int _dataBufferSize = 4096;
+        public int id;
+        public TCP tcp;
+    
+        public Client(int _clientId)
+        {
+            id = _clientId;
+            tcp = new TCP(id);
+        }
+        
+        public class TCP
+        {
+            public TcpClient socket;
+            private readonly int id;
+    
+            private NetworkStream _stream;
+            private byte[] _receiveBuffer;
+            public TCP(int _id)
+            {
+                id = _id;
+            }
+    
+            public void Connect(TcpClient _socket)
+            {
+                socket = _socket;
+                socket.ReceiveBufferSize = _dataBufferSize;
+                socket.SendBufferSize = _dataBufferSize;
+    
+                _stream = socket.GetStream();
+    
+                _receiveBuffer = new byte[_dataBufferSize];
+    
+                _stream.BeginRead(_receiveBuffer, 0, _dataBufferSize, ReceiveCallBack, null);
+            }
+    
+            private void ReceiveCallBack(IAsyncResult _result)
+            {
+                try
+                {
+                    int _byteLength = _stream.EndRead(_result);
+    
+                    if (_byteLength <= 0)
+                    {
+                        return;
+                    }
+    
+                    byte[] _data = new byte[_byteLength];
+                    Array.Copy(_receiveBuffer, _data, _byteLength);
+    
+                    _stream.BeginRead(_receiveBuffer, 0, _dataBufferSize, ReceiveCallBack, null);
+                }
+                catch (Exception _ex)
+                {
+                    Debug.Log($"Error receiving TCP data: {_ex} ");
+                }
+            }
+        }  
+    }
+}
+
+
---
+++ Assets/Scripts/Server.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using GameServer;
+using UnityEngine;
+
+public class Server : MonoBehaviour
+{
+    public static int MaxPlayers { get; private set; }
+    public static int Port { get; private set; }
+
+    public static Dictionary<int, Client> clients = new Dictionary<int, Client>();
+
+    private static TcpListener _tcpListener;
+    void Start()
+    {
+        Port = 26950;
+        MaxPlayers = 10;
+        StartServer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void StartServer()
+    {
+        Debug.Log("Starting server...");
+        InitializeServerData();
+
+        _tcpListener = new TcpListener(IPAddress.Any, Port);
+        _tcpListener.Start();
+        _tcpListener.BeginAcceptTcpClient(new AsyncCallback(TcpConnectCallback), null);
+        
+        Console.WriteLine($"Server started on {Port}.");
+    }
+
+    private void TcpConnectCallback(IAsyncResult _result)
+    {
+        TcpClient _client = _tcpListener.EndAcceptTcpClient(_result);
+        _tcpListener.BeginAcceptTcpClient(new AsyncCallback(TcpConnectCallback), null);
+        
+        Debug.Log($"Incoming conection from {_client.Client.RemoteEndPoint}...");
+        
+        for (int i = 0; i <= MaxPlayers; i++)
+        {
+            if (clients[i].tcp.socket == null)
+            {
+               clients[i].tcp.Connect(_client);
+               return;
+            }
+        }
+        Debug.Log($"{_client.Client.RemoteEndPoint} failed to connect : Server full! ");
+    }
+
+    private static void InitializeServerData()
+    {
+        for (int i = 0; i <= MaxPlayers; i++)
+        {
+            clients.Add(i, new Client(i));
+        }
+    }
+}
